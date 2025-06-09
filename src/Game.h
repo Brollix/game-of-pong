aec7b552f9f5f2211ecd2f0ec679933d27dc63a8
Agus Brollo <brolloagus@gmail.com>
2025-06-09 07:42:44 -0300

    Enhance Game and NeuralNet functionality
    Significantly modified `Game.h` to add support for a second player and AI, including new member variables, methods for game state management, and improved scoring and fitness evaluation. Introduced `NeuralNet.h` with a complete neural network implementation, including methods for forward propagation, AI decision-making, and weight management, enhancing the AI's learning capabilities.

--- src/Game.h
+++ src/Game.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#include "Utils.h"
-#include "Collisions.h"
-//#include "Text.h"
-#include "Menu.h"
-#include "HUD.h"
-#include "Player.h"
-#include "Ball.h"
-
 enum class GameState {
 	MainMenu,
 	OptionsMenu,
@@ -19,20 +11,27 @@ enum class GameState {
 #pragma region Game
 
 struct Game {
+
 #pragma region Miembros
 
 	int width, height;
+	int maxScore = 5;
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 	TTF_Font* font = nullptr;
+	TTF_Font* roboto = nullptr;
 
 	GameState state = GameState::MainMenu;
 
 	HUD* hudDisplay = nullptr;
+	Text* fitnessInfo = nullptr;
+	Text* aiInfoText = nullptr;
 
 	Menu* pauseMenu = nullptr;
 	Menu* mainMenu = nullptr;
 	Player player;
+	Player player2;
+	NeuralNet ai;
 	Ball* ball = nullptr;
 
 #pragma endregion
@@ -41,7 +40,8 @@ struct Game {
 
 	Game(int w, int h)
 		: width(w), height(h),
-		player(35, h / 2, 15, 100) {
+		player(35, h / 2, 15, 100, { 255, 65, 65, 255 }),
+		player2(width - 35, h / 2, 15, 100) {
 	}
 
 	~Game() {
@@ -55,33 +55,34 @@ struct Game {
 	bool init() {
 		if (!initSDL()) return false;
 
+		if (ai.loadFromFile("ai_data")) {
+			if (ai.bestNet) ai.copyFrom(*ai.bestNet);
+		}
+
+		TTF_Font* robotoFont = TTF_OpenFont("assets/roboto.ttf", 18);
+		if (!robotoFont) {
+			cerr << "Error cargando Roboto: " << TTF_GetError() << endl;
+			return false;
+		}
+
+		fitnessInfo = new Text(renderer, robotoFont);
+		aiInfoText = new Text(renderer, robotoFont, { 20, 60 });
+
 		ball = new Ball(renderer);
 
-		pauseMenu = new Menu(
-			renderer,
-			{
+		pauseMenu = new Menu( renderer, {
 				width / 4, height / 4,
 				width / 2, height / 2
-			},
-			{
-				"Continue",
-				"Options",
-				"Main Menu"
-			},
+			}, {
+				"Continue",	"Options", "Main Menu" },
 			font
 		);
 
-		mainMenu = new Menu(
-			renderer,
-			{
+		mainMenu = new Menu( renderer, {
 				width / 4, height / 4,
 				width / 2, height / 2
-			},
-			{
-				"Start Game",
-				"Options",
-				"Quit"
-			},
+			}, {
+				"Start Game", "Options", "Quit"	},
 			font
 		);
 
@@ -92,15 +93,15 @@ struct Game {
 
 	bool initSDL() {
 		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-			std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+			cerr << "SDL_Init Error: " << SDL_GetError() << endl;
 			return false;
 		}
 		if (TTF_Init() == -1) {
-			std::cerr << "TTF_Init Error: " << TTF_GetError() << std::endl;
+			cerr << "TTF_Init Error: " << TTF_GetError() << endl;
 			return false;
 		}
 		if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-			std::cerr << "Mix_OpenAudio Error: " << Mix_GetError() << std::endl;
+			cerr << "Mix_OpenAudio Error: " << Mix_GetError() << endl;
 			return false;
 		}
 
@@ -108,19 +109,19 @@ struct Game {
 			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 			width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 		if (!window) {
-			std::cerr << "CreateWindow Error: " << SDL_GetError() << std::endl;
+			cerr << "CreateWindow Error: " << SDL_GetError() << endl;
 			return false;
 		}
 
 		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 		if (!renderer) {
-			std::cerr << "CreateRenderer Error: " << SDL_GetError() << std::endl;
+			cerr << "CreateRenderer Error: " << SDL_GetError() << endl;
 			return false;
 		}
 
 		font = TTF_OpenFont("assets/pong.ttf", 24);
 		if (!font) {
-			std::cerr << "TTF_OpenFont Error: " << TTF_GetError() << std::endl;
+			cerr << "TTF_OpenFont Error: " << TTF_GetError() << endl;
 			return false;
 		}
 
@@ -130,7 +131,7 @@ struct Game {
 
 #pragma endregion
 
-#pragma region Loop
+#pragma region MainLoop
 
 	void run() {
 		Uint32 lastTime = SDL_GetTicks();
@@ -206,21 +207,53 @@ struct Game {
 
 #pragma endregion
 
-#pragma region Lógica
+#pragma region Game Loop
 
 	void update(float dt) {
 		if (state != GameState::Playing) return;
 
-		player.move(height, dt);
-		ball->move(dt, width, height);
+		player.move(dt);
+		ai.update(player2, *ball, dt, player.pos.y);
 
-		if (Collisions::check(ball->getFRect(), player.getFRect())) {
-		    ball->dir.x *= -1;
+		stringstream ss;
+		ss 
+			<< "Fitness: " 
+			<< ai.fitness 
+			<< "  Best: " 
+			<< ai.bestFitness 
+			<< "  Gen w/o imp: " 
+			<< ai.gensNoImprovement;
+
+		aiInfoText->setString(ss.str());
+
+		ball->move(dt);
+
+
+		if (Collisions::check(ball->getFRect(), player.getFRect()) ||
+			Collisions::check(ball->getFRect(), player2.getFRect())) {
+			ball->vel.x *= -1;
 		}
 
-		if (true) {
-
+		if (Collisions::check(ball->getFRect(), player2.getFRect())) {
+			ai.fitness += 50;
 		}
+
+		if (ball->pos.x + ball->size.x < 0) {
+			player2.score++;
+			hudDisplay->setScore(player.score, player2.score);
+			resetRound();
+		} else if (ball->pos.x > width) {
+			player.score++;
+			hudDisplay->setScore(player.score, player2.score);
+			ai.penalizeForGoal();
+			resetRound();
+		}
+
+		if (player.score >= maxScore || player2.score >= maxScore) {
+			ai.evaluateAndEvolve();
+			resetGame();
+		}
+
 	}
 
 #pragma endregion
@@ -237,13 +270,20 @@ struct Game {
 				break;
 			case GameState::Playing:
 				player.render(renderer);
+				player2.render(renderer);
 				ball->render();
 				hudDisplay->render();
+				aiInfoText->render();
+
+
 				break;
 			case GameState::Paused:
 				player.render(renderer);
+				player2.render(renderer);
 				ball->render();
 				renderPauseOverlay();
+				fitnessInfo->render();
+
 				break;
 			default:
 				break;
@@ -251,7 +291,6 @@ struct Game {
 
 		SDL_RenderPresent(renderer);
 		SDL_GL_SwapWindow(window);
-
 	}
 
 	void renderMainMenu() {
@@ -271,6 +310,27 @@ struct Game {
 		if (pauseMenu) pauseMenu->render();
 	}
 
+	void resetRound() {
+		ball->pos = { width / 2.0f, height / 2.0f };
+		ball->vel = randomVec2iFixed() * ball->speed;
+		ai.loadAndApplyBest();
+		ai.evaluateAndEvolve();
+	}
+
+	void resetGame() {
+		// Reset puntuación
+		player.score = 0;
+		player2.score = 0;
+		hudDisplay->setScore(player.score, player2.score);
+
+		// Reset posición de paletas
+		player.pos.y = height / 2.0f - player.size.y / 2.0f;
+		player2.pos.y = height / 2.0f - player2.size.y / 2.0f;
+
+		// Reset pelota
+		resetRound();
+	}
+
 #pragma endregion
 
 #pragma region Limpieza
@@ -278,6 +338,8 @@ struct Game {
 	void clean() {
 		if (pauseMenu) delete pauseMenu;
 		if (ball) delete ball;
+		if (fitnessInfo) delete fitnessInfo;
+
 		if (font) TTF_CloseFont(font);
 
 		if (renderer) SDL_DestroyRenderer(renderer);
---
+++ src/NeuralNet.h
@@ -0,0 +1,227 @@
+#pragma once
+
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <string>
+
+#include "Utils.h"
+#include "Ball.h"
+#include "Player.h"
+#include "FileIO.h"
+
+using namespace std;
+
+struct NeuralNet {
+    int inputSize;
+    int hiddenSize;
+
+    int gensNoImprovement = 0;
+
+    vector<vector<float>> hiddenWeights;
+    vector<float> hiddenBias;
+    vector<float> outputWeights;
+    float outputBias = 0.0f;
+
+    float fitness = 0.0f;
+    float bestFitness = -INFINITY;
+
+    float lastY = 0.0f;
+    NeuralNet* bestNet = nullptr;
+
+    NeuralNet(int inputSize = 9, int hiddenSize = 8, bool createBest = true)
+        : inputSize(inputSize), hiddenSize(hiddenSize) {
+        hiddenWeights.resize(hiddenSize, vector<float>(inputSize));
+        hiddenBias.resize(hiddenSize);
+        outputWeights.resize(hiddenSize);
+        randomizeWeights();
+
+        if (createBest){
+            bestNet = new NeuralNet(inputSize, hiddenSize, false);
+        }
+    }
+
+    ~NeuralNet() {
+        delete bestNet;
+    }
+
+    float forward(const vector<float>& input) {
+        vector<float> hiddenOutput(hiddenSize);
+        for (int i = 0; i < hiddenSize; ++i) {
+            float sum = hiddenBias[i];
+            for (int j = 0; j < inputSize; ++j) {
+                sum += hiddenWeights[i][j] * input[j];
+            }
+            hiddenOutput[i] = tanh(sum);
+        }
+
+        float output = outputBias;
+        for (int i = 0; i < hiddenSize; ++i) {
+            output += hiddenOutput[i] * outputWeights[i];
+        }
+        return output;
+    }
+
+    void update(Player& paddle, const Ball& ball, float dt, float playerY) {
+        vector<float> input;
+        appendVec2f(input, ball.pos);
+        appendVec2f(input, ball.vel);
+        appendVec2f(input, normalize(ball.vel));
+        appendVec2f(input, ball.pos - paddle.pos);
+        input.push_back(playerY / height);  // Normalizamos la posición del jugador humano
+
+
+        float output = forward(input);
+
+        if (output > 0.1f) paddle.moveUp(dt);
+        else if (output < -0.1f) paddle.moveDown(dt);
+
+        paddle.clamp(height);
+
+        float movement = abs(paddle.pos.y - lastY);
+        if (movement < 1.0f)
+            fitness -= dt * 0.5f;
+
+        lastY = paddle.pos.y;
+
+        float distY = abs((ball.pos.y + ball.size.y * 0.5f) - (paddle.pos.y + paddle.size.y * 0.5f));
+        fitness += (1.0f - (distY / height)) * dt;
+        fitness += dt;
+    }
+
+    void penalizeForGoal() {
+        fitness -= 10.0f;
+    }
+
+    void evaluateAndEvolve() {
+        if (fitness > bestFitness) {
+            bestFitness = fitness;
+            gensNoImprovement = 0;
+            bestNet->copyFrom(*this);
+            bestNet->saveToFile("ai_data");
+        } else {
+            gensNoImprovement++;
+            copyFrom(*bestNet);
+            mutate();
+        }
+
+    }
+
+    void randomizeWeights(float min = -1.0f, float max = 1.0f) {
+        srand(static_cast<unsigned int>(time(nullptr)));
+        for (auto& row : hiddenWeights){ 
+                for (auto& w : row){
+                    w = randFloat(min, max);
+                    cout << w << endl;
+            } 
+        }
+
+        for (auto& b : hiddenBias)
+            b = randFloat(min, max);
+
+        for (auto& w : outputWeights)
+            w = randFloat(min, max);
+
+        outputBias = randFloat(min, max);
+    }
+
+    void mutate(float rate = 0.1f) {
+        for (auto& row : hiddenWeights)
+            for (auto& w : row)
+                w += randFloat(-rate, rate);
+
+        for (auto& b : hiddenBias)
+            b += randFloat(-rate, rate);
+
+        for (auto& w : outputWeights)
+            w += randFloat(-rate, rate);
+
+        outputBias += randFloat(-rate, rate);
+    }
+
+    void copyFrom(const NeuralNet& other) {
+        hiddenWeights = other.hiddenWeights;
+        hiddenBias = other.hiddenBias;
+        outputWeights = other.outputWeights;
+        outputBias = other.outputBias;
+    }
+
+    float relu(float x) const {
+        return x > 0.0f ? x : 0.0f;
+    }
+
+    bool loadFromFile(const string& filename) {
+        cout << fixed << setprecision(3);
+        cout << "[AI] Loaded weights from file: " << filename << endl;
+
+        vector<float> flatWeights;
+        if (!FileIO::loadVector(filename + ".hiddenWeights", flatWeights)) return false;
+        if (!FileIO::loadVector(filename + ".hiddenBias", hiddenBias)) return false;
+        if (!FileIO::loadVector(filename + ".outputWeights", outputWeights)) return false;
+        if (!FileIO::load(filename + ".outputBias", outputBias)) return false;
+
+        int index = 0;
+        for (auto& row : hiddenWeights)
+            for (float& w : row)
+                w = flatWeights[index++];
+
+        cout << "HiddenWeights:\n";
+        for (const auto& row : hiddenWeights) {
+            for (float w : row) cout << setw(7) << w << " ";
+            cout << endl;
+        }
+
+        cout << "HiddenBias:\n";
+        for (float b : hiddenBias) cout << setw(7) << b << " ";
+        cout << endl;
+
+        cout << "OutputWeights:\n";
+        for (float w : outputWeights) cout << setw(7) << w << " ";
+        cout << endl;
+
+        cout << "OutputBias: " << setw(7) << outputBias << endl;
+
+        return true;
+    }
+
+    bool saveToFile(const string& filename) const {
+        cout << fixed << setprecision(3);
+        cout << "[AI] Saved weights to file: " << filename << endl;
+
+        cout << "HiddenWeights:\n";
+        for (const auto& row : hiddenWeights) {
+            for (float w : row) cout << setw(7) << w << " ";
+            cout << endl;
+        }
+
+        cout << "HiddenBias:\n";
+        for (float b : hiddenBias) cout << setw(7) << b << " ";
+        cout << endl;
+
+        cout << "OutputWeights:\n";
+        for (float w : outputWeights) cout << setw(7) << w << " ";
+        cout << endl;
+
+        cout << "OutputBias: " << setw(7) << outputBias << endl;
+
+        return FileIO::saveVector(filename + ".hiddenWeights", flatten2D(hiddenWeights)) &&
+            FileIO::saveVector(filename + ".hiddenBias", hiddenBias) &&
+            FileIO::saveVector(filename + ".outputWeights", outputWeights) &&
+            FileIO::save(filename + ".outputBias", outputBias);
+    }
+
+
+    vector<float> flatten2D(const vector<vector<float>>& matrix) const {
+        vector<float> flat;
+        for (const auto& row : matrix)
+            flat.insert(flat.end(), row.begin(), row.end());
+        return flat;
+    }
+
+    void loadAndApplyBest() {
+        if (bestNet && bestNet->loadFromFile("ai_data")) {
+            copyFrom(*bestNet);
+        }
+    }
+};
